ce3bccc4620c2ea4d3d94c13a4e2313361fcdcac
Matheus Grandinetti <matheusgrandinetti@gmail.com>
2025-06-07 12:56:45 -0300

    repo deletions staged

--- buzzed/stores/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from stores.views import CreateStore, UpdateStore
-
-urlpatterns = [
-    path('<int:pk>/update-store', UpdateStore.as_view(), name='update_store')
-]
--- buzzed/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from users.views import LoginView, SignupView
-
-urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login')
-]
